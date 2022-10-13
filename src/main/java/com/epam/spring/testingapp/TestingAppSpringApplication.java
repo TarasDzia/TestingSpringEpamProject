d8351a4b56201a6fcc49797bb7d2f12475e9a609
Taras <taras28dz@gmail.com>
2022-10-13 12:54:19 +0300

    pom.xml

---
+++ src/main/java/com/epam/spring/testingapp/TestingAppSpringApplication.java
@@ -0,0 +1,11 @@
+package com.epam.spring.testingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingAppSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestingAppSpringApplication.class, args);
+    }
+}
